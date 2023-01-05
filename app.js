8d58b2b8a2d619d06519d242acff86bb887e5a65
Ömer ŞAHİN <omers4hin04@gmail.com>
2023-01-05 23:32:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-// const x=process.argv.slice(2)
 
-
-
-// isprime=(min,max)=>{
-//     for(let i=min;i<=max;i++){
-//         let isnumber=true
-//         for(let j=2;j<=i;j++){
-//             if(i%j==0 && j!=i){
-//                 isnumber=false
-//             }
-//         }
-
-//         if(isnumber){
-//             console.log(i)
-//         }
-//     }
-
-
-// }
-
-
-
-// isprime(x[0]*1,x[1]*1)
 
 const circle=process.argv.slice(2)[0]*1;
 
