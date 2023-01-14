9992a7e2af924cde951a5d6e3aeeeffe9eab5a24
Ömer ŞAHİN <omers4hin04@gmail.com>
2023-01-14 22:37:11 +0300

    Daire Modüler Dosyası

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-
-
-const circle=process.argv.slice(2)[0]*1;
-
-
-calculate =(circle)=>{
-
-    let area=(Math.PI)*(Math.pow(circle,2))
-    console.log(`The area of ​​a circle with a radius of ${circle} is ${area}` )
-
-}
-
-
-calculate(circle)
+const {circleArea,circleCircumference}=require("./circle")
+circleArea(5);
+circleCircumference(5)
---
+++ circle.js
@@ -0,0 +1,19 @@
+const circleArea=(radius)=>{
+ let area=radius*radius*Math.PI
+
+
+    console.log(area)
+
+}
+
+
+const circleCircumference=(radius)=>{
+
+    let circumference=2*Math.PI*radius
+    console.log(circumference)
+
+}
+module.exports={
+    circleArea,
+    circleCircumference
+}
