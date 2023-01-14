01aa1d4b2436396092ba640dcb51e0895aa2fa9d
Ömer ŞAHİN <omers4hin04@gmail.com>
2023-01-15 00:45:42 +0300

    FS File System Modülü CRUD

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
-const {circleArea,circleCircumference}=require("./circle")
-circleArea(5);
-circleCircumference(5)
+const fs=require("fs")
+
+
+setTimeout(() => {
+    fs.writeFile("employees.json",'{"Name":"Employee 1 Name","Salary":2000}',(err)=>{
+        if(err) console.log(err)
+    
+        console.log("Employees.json Başarılı bir şekilde oluşturuldu")
+    
+    })
+    
+    
+}, 1000);
+
+
+
+setTimeout(() => {
+    fs.readFile("employees.json","utf-8",(err,data)=>{
+        if(err) console.log(err)
+        console.log(data)
+    
+    })
+    
+    
+}, 2000);
+
+setTimeout(() => {
+    fs.writeFile("employees.json",'{"Name":"Employee 2 Name","Salary":2000}',(err)=>{
+        if(err) console.log(err)
+    
+        console.log("Employees.json Başarılı bir şekilde oluşturuldu")
+    
+    })
+    
+    
+}, 3000);
+
+
+
+setTimeout(() => {
+    fs.unlink("employees.json",(error)=>{
+
+
+        if(error) console.log("Bir hata oluştu")
+    
+    
+        console.log("Dosyta başarılı bir şekilde silindi")
+    })
+    
+}, 4000);
+
